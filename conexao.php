5a85f92f9b92a27682f7fcf3e89b38e90ab2a236
Ana Karoline <anadevkarol@gmail.com>
2024-03-20 18:07:47 -0300

    conectando ao bd e inserindi pages

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 
 $servidor = "localhost";
-$banco = "ci3";
+$banco = "nutriacaoweb";
 $usuariobd = "root";
 $senhabd = "";
 
@@ -9,4 +9,5 @@ $conect = new mysqli($servidor,$usuariobd,$senhabd, $banco);
 if ($conect->connect_errno) {
 	die("Falha ao conectar: ".$conect->connect_error);
 }
+    echo "conectado com sucesso";
 ?> 
