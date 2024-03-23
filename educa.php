650a0391750eb515f4a85cb00997c7d0d572c2ec
Ana Karoline <anadevkarol@gmail.com>
2024-03-23 12:52:42 -0300

    alterações na tela educa

--- educa.php
+++ educa.php
@@ -4,6 +4,8 @@
  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>NutriAcao</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" href="style/style.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="style/style.css">
 
 <div class="navbar">
@@ -19,6 +21,57 @@
       <li><a href="junte-se.php">Junte-se a Nós</a></li>
     </ul>
   </div>
+  <div class="title">
+    <h1 style='text-align: center'>NutriAcao educa</h1>
+  </div>
+  <br>
+  <div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner" style="height: 400px">
+    <div class="carousel-item active">
+      <img src="assets/img1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img2.webp" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img3.webp" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<br>
+<div class="button-container" style="text-align: center;">
+  <a class="btn btn-primary" href="login.php" role="button" style="margin-right: 30px; background-color: #712584;border:none;">Notícias</a>
+  <a class="btn btn-primary" href="login.php" role="button" style="margin-right: 30px; background-color: #712584; border:none">Dicas</a>
+  <a class="btn btn-primary" href="login.php" role="button" style="margin-right: 30px; background-color: #712584; border:none;">Receitas</a>
+</div>
+<br>
+<br>
+<div style='background-color:#712584; padding: 50px 0;'>
+    <footer style='color: #333; text-decoration: none; margin: 0px;'>
+        <p style='color:white; margin: 20px; font-weight: bold;'>NutriAcao </p>
+        <nav>
+            <a href="#" style='color: #fff; text-decoration: none; margin: 0 px;'><img src="" alt="Logo Rede Social"></a>
+            <a href="#">Termos de Serviço</a>
+            <a href="#">Política de Privacidade</a>
+            <a href="#">Contato</a>
+        </nav>
+    </footer>
+</div>
+<br>
+
 </head>
 </html>
   
