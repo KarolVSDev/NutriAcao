c6c1c04ca37c0528ad491bc87fe8665fe5fa4aa9
Ana Karoline <anadevkarol@gmail.com>
2024-05-20 13:43:56 -0300

    update

---
+++ home.php
@@ -0,0 +1,135 @@
+<?php include('config.php'); ?>
+<!DOCTYPE html>
+<head>
+ <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>NutriAcao</title>
+  <link rel="stylesheet" href="style/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="navbar">
+    <div class="logo-container">
+      <img src="assets/logo.jpeg" alt="Logo" class="logo-img">
+      <span class="company-name"><strong>Nutri</strong>Ação</span>  
+    </div>
+    <ul class="nav-links">
+      <li><a href="home.php">Home</a></li>
+      <li><a href="quem-somos.php">Quem Somos</a></li>
+      <li><a href="educa_noticias.php">Educa</a></li>
+      <li><a href="junte-se.php">Junte-se a Nós</a></li>
+    </ul>
+  </div>
+  <img src="assets/home.png" class="img-fluid" width='100%' height="300px">
+  <br>
+  <br>
+  <div class="card-group">
+  <div class="card" class="card"  style='background:#9B37B5'>
+    <div class="card-body">
+      <h5 class="card-title" style='text-align:center; color: white;'>62,5%</h5>
+      <p class="card-text" style='text-align:center; color: white;'>da população do Amazonas encontram-se  em Insegurança Alimentar</p>
+    </div>
+  </div>
+  <div class="card" style='background: #712584'>
+    <div class="card-body">
+      <h5 class="card-title" style='text-align:center; color: white;'>90.000  kg</h5>
+      <p class="card-text" style='text-align:center; color: white;'>de alimentos são desperdiçados em Manaus</p>
+      <p class="card-text" style='text-align:center'><small class="text-body-secondary">*dados do IBGE de 2017/2018</small></p>
+    </div>
+  </div>
+  <div class="card"  style='background:#9B37B5'>
+    <div class="card-body">
+      <h5 class="card-title" style='text-align:center; color: white;'>lei nº 5.297</h5>
+      <p class="card-text" style='text-align:center; color: white;'>Combate ao desperdício e à perda de alimentos no âmbito do Estado do Amazonas</p>
+    </div>
+  </div>
+</div>
+<br>
+<br>
+<div class='title'>
+  <h1>NutriAcao Educa</h1>
+</div>
+<div class='subtitle'>
+  <h6 style='text-align:center; color: #712584'>Notícias, dicas, receitas e video aulas para voce se tornar um expert no combate ao desperdício de alimentos.</h6>
+</div>
+<br>
+  <br>
+<div class="card" style=" border: none; display: inline-block;width: 30%;margin: 10px;position: relative;text-align: center;">
+  <img style=" width:60%;height: auto;" src="assets/receitas.jpg" alt="Placeholder Image">
+  <h3 class="card-title" style=' position: absolute;top: 50%; left: 50%; transform: translate(-50%, -50%);color: white;font-size: 24px;'>Reaproveitamento de alimentos: veja receitas fáceis para economizar</h3>
+</div>
+
+<div class="card" style="border: none; display: inline-block;width: 30%;margin: 10px;position: relative;text-align: center;">
+  <img style="width: 60%;height: auto;" src="assets/inseguranca.jpg" alt="Placeholder Image">
+  <h3 class="card-title" style=' position: absolute;top: 50%; left: 50%; transform: translate(-50%, -50%);color: white;font-size: 24px;'>A Insegurança Alimentar Nutricional no Amazonas</h3>
+</div>
+
+<div class="card" style="border:none; display: inline-block;width: 30%;margin: 10px;position: relative;text-align: center;">
+  <img style="width: 60%;height: auto;" src="assets/reaproveitar.jpeg" alt="Placeholder Image">
+  <h3 class="card-title" style=' position: absolute;top: 50%; left: 50%; transform: translate(-50%, -50%);color: white;font-size: 24px;'>10 dicas para não desperdiçar seu alimento</h3>
+</div>
+<br>
+<div class="container d-flex justify-content-center mt-5">
+  <a href="educa_noticias.php" class="btn btn-primary border-0" style="background-color: #712584;">CONHEÇA</a>
+</div>
+<br>
+<br>
+<div class="container text-center" style="color: #712584;">
+  <h4 >Como funciona?</h4>
+</div>
+
+<div class="container d-flex justify-content-center mt-5">
+  <div>
+    <img src="assets/NutriAcao.roadmapwb.png" class="img-fluid" width="100%" height="80px">
+  </div>
+</div>
+<br>
+<br>
+<div class="container text-center" style="color: #712584;">
+  <h4> Quem já contribuiu nessa luta?</h4>
+</div>
+<br>
+<div class="card-group">
+  <div class="card" style="border: none;">
+    <img src="assets/parceiro1.png" class="card-img-top" style="max-width: 45%; height: auto;" alt="">
+  </div>
+  <div class="card" style="border: none;">
+    <img src="assets/parceiro2.jpg" class="card-img-top" style="max-width: 45%; height: auto;" alt="...">
+  </div>
+  <div class="card" style="border: none;">
+    <img src="assets/parceiro3.png" class="card-img-top" style="max-width: 50%; height: auto;" alt="...">
+  </div>
+  <div class="card" style="border: none;">
+    <img src="assets/parceiro4.jpg" class="card-img-top" style="max-width: 50%; height: auto;" alt="...">
+  </div>
+  <div class="card" style="border: none;">
+    <img src="assets/parceiro4.jpg" class="card-img-top" style="max-width: 50%; height: auto;" alt="...">
+  </div>
+</div>
+<br>
+<br>
+<footer>
+<div class='rodape'><nav>
+  <div class="div1">
+      <p><strong>Nutri</strong>Ação</p>
+    </div>
+    <div class="div2">
+            <a href="#">Termos de Serviço |</a>
+            <a href="#">Política de Privacidade</a>    
+  </div>
+    <div class="div3"><p>
+  <ion-icon name="logo-youtube"></ion-icon>
+  <ion-icon name="logo-instagram"></ion-icon>
+  <ion-icon name="logo-facebook"></ion-icon></p>
+  </div>
+  </nav>
+</div>
+<div class="rodape2">
+    <P>Feito por <i>GPROGM</i></P>
+  </div>
+  </footer>
+<script  type = "module"  src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"> </script> 
+<script  nomodule  src = "https://unpkg .com/ionicons@7.1.0/dist/ionicons/ionicons.js"> </script>
+</body>
+</html>
