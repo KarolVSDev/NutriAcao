b1f68dd80658c2fdc57876bcfeaf42ef7198c996
Ana Karoline <anadevkarol@gmail.com>
2024-05-23 20:35:21 -0300

    atualizando páginas privadas

--- ecucaproreceitas.php
+++ ecucaproreceitas.php
@@ -1,92 +1,491 @@
 <!DOCTYPE html>
+
 <head>
+
  <meta charset="UTF-8">
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>NutriAcao</title>
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" href="style/style.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer"
+ />
+
   <link rel="stylesheet" href="style/style.css">
+
 </head>
+
 <body>
+
 <div class="navbar">
+
     <div class="logo-container">
+
       <img src="assets/logo.jpeg" alt="Logo" class="logo-img">
+
       <span class="company-name"><strong>Nutri</strong>Ação</span>
+
       
+
     </div>
+
     <ul class="nav-links">
+
       <li><a href="home.php">Home</a></li>
+
       <li><a href="quem-somos.php">Quem Somos</a></li>
+
       <li><a href="educa_noticias.php">Educa</a></li>
+
       <li><a href="junte-se.php">Junte-se a Nós</a></li>
+
     </ul>
-  </div>
-  <div style="display: flex;justify-content: end;align-items: end;">
-            
-            <div class="caixa-pesquisa" style="width:400px ;
-    height: 50px;
-    position: relative;
-    box-shadow:0 0 10px rgba(0, 0, 0, 0.34);border-radius:25px">
-                <input type="text" placeholder="Digite aqui..." style="height: 100%;
-  width:100% ;
-  border-radius: 25px;
-  outline: none;
-  border: none;
-  padding-left: 20px;
-  font-size: 18px;">
-                <label for="check" class="iconp" style="position:absolute;right:0;top:0;height:100%;width:50px;background-color:#fff;font-size:18px;text-align:center;line-height:50px;background-color:#712584;border-radius:0 25px 25px;color:#fff">
-                    <i class="fas fa-search"></i>
-                </label>
-            </div>
-</div>
-  <div class="title">
-    <h1><strong>Nutri</strong>Ação educa</h1>
-  </div>
-  
-  <a href="https://www.nsctotal.com.br/noticias/projeto-da-ufsc-de-blumenau-ensina-a-evitar-desperdicio-de-alimentos-veja-o-material"><img  src="assets/imgreceitas.png" alt="" width=100%;></a>
-  <br><br>
-<div class='buttons'>
-    <a class="btn btn-primary" href="educapro.php" role="button"> <ion-icon name="earth-outline"></ion-icon> Notícias</a>
-    <a class="btn btn-primary" href="ecucaproreceitas.php" role="button"><ion-icon name="restaurant-outline"></ion-icon> Receitas</a>
-    <a class="btn btn-primary" href="educaprodicas.php" role="button"><ion-icon name="newspaper-outline"></ion-icon> Dicas</a>
-    <br>
-    <br>
+
   </div>
 
+  <div style="display: flex;justify-content: end;align-items: end;">
+
+            
+
+            <div class="caixa-pesquisa" style="width:400px ;
+
+    height: 50px;
+
+    position: relative;
+
+    box-shadow:0 0 10px rgba(0, 0, 0, 0.34);border-radius:25px">
+
+                <input type="text" placeholder="Digite aqui..." style="height: 100%;
+
+  width:100% ;
+
+  border-radius: 25px;
+
+  outline: none;
+
+  border: none;
+
+  padding-left: 20px;
+
+  font-size: 18px;">
+
+                <label for="check" class="iconp" style="position:absolute;right:0;top:0;height:100%;width:50px;background-color:#fff;font-size:18px;text-align:center;line-height:50px;background-color:#712584;border-radius:0 25px 25px;color:#fff">
+
+                    <i class="fas fa-search"></i>
+
+                </label>
+
+            </div>
+
+</div>
+
+<br>
+
+  <br>
+
+<img  src="assets/educapro.png" alt="" width=100%;>
+
+  <br>
+
+  <br>
+
+<div class='buttons'>
+
+    <a class="btn btn-primary" href="educapro.php" role="button"> <ion-icon name="earth-outline"></ion-icon> Notícias</a>
+
+    <a class="btn btn-primary" href="ecucaproreceitas.php" role="button"><ion-icon name="restaurant-outline"></ion-icon> Receitas</a>
+
+    <a class="btn btn-primary" href="educaprodicas.php" role="button"><ion-icon name="newspaper-outline"></ion-icon> Dicas</a>
+
+    <br>
+
+    <br>
+
+  </div>
+
+
+
+
 <div class="holder" style=" display: flex;justify-content: center;">
-  <a href="https://www.youtube.com/watch?v=HyIdAFW0wN8&pp=ygUfcmVjZWl0YXMgY29tIGEgY2FzY2EgZGUgbGFyYW5qYQ%3D%3D">
-    <img class="imagemr" src="assets/receita1.png" alt="" style="width: 80%;border-radius: 5%;margin: 10px;margin-left: 15%;margin-top: 3%;position: relative;text-align: center;">
-  </a>
-  <a href="https://www.unileverfoodsolutions.com.br/dicas-e-servicos/tendencias-no-prato/desperdicio-ebook-1.html">
-    <img class="imagemr" src="assets/receita2.png" alt="" style="width: 80%;border-radius: 5%;margin: 10px;margin-left: 15%;margin-top: 3%;position: relative;text-align: center;">
-  </a>
-</div>
+
+    <img class="imagemr" src="assets/sopatalo.jpg" alt="" style="width: 30%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
   
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Sopa de Talos e Folhas</b> <br>
+
+Ingredientes:
+
+<br>
+
+Talos de couve, brócolis, espinafre, etc.
+
+Folhas de beterraba, cenoura, nabo, etc.
+
+1 cebola
+
+2 dentes de alho
+
+1 litro de água ou caldo de legumes
+
+Sal e pimenta a gosto
+
+Azeite<br><br>
+
+Modo de Preparo:<br>
+
+Pique os talos e folhas.
+
+Refogue a cebola e o alho no azeite.
+
+Adicione os talos e folhas e refogue por alguns minutos.
+
+Acrescente a água ou caldo de legumes, sal e pimenta.
+
+Cozinhe até os talos estarem macios e bata no liquidificador.
+
+Ajuste os temperos e sirva.</p>
+
+</div>
+
+<br>
+
+<div class="holder" style=" display: flex;justify-content: center;">
+
+    <img class="imagemr" src="assets/chipbatata.jpg" alt="" style="width: 30%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Chips de Casca de Batata</b><br>
+
+Ingredientes:<br>
+
+
+
+
+Cascas de batata
+
+Azeite
+
+Sal e especiarias a gosto<br><br>
+
+Modo de Preparo:<br>
+
+
+
+
+Lave bem as cascas de batata.
+
+Misture com azeite, sal e especiarias.
+
+Asse em forno pré-aquecido a 180°C até ficarem crocantes.</p>
+
+  </div>
+
+  <div class="holder" style=" display: flex;justify-content: center;">
+
+    <img class="imagemr" src="assets/bolocascabanana.jpg" alt="" style="width: 30%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Bolo de Casca de Banana</b><br>
+
+Ingredientes:<br>
+
+
+
+
+4 cascas de banana
+
+3 ovos
+
+2 xícaras de açúcar
+
+2 colheres (sopa) de manteiga
+
+2 xícaras de farinha de rosca
+
+1 colher (sopa) de fermento em pó<br><br>
+
+Modo de Preparo:<br>
+
+
+
+
+Bata as cascas de banana com um pouco de água no liquidificador.
+
+Acrescente os ovos, o açúcar e a manteiga, batendo bem.
+
+Misture a farinha de rosca e o fermento.
+
+Asse em forno pré-aquecido a 180°C por cerca de 40 minutos.</p>
+
+  </div>
+
+  <div class="holder" style=" display: flex;justify-content: center;">
+
+    <img class="imagemr" src="assets/pestocenoura.jpg" alt="" style="width: 30%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Pesto de Folhas de Cenoura</b><br>
+
+Ingredientes:<br>
+
+
+
+
+Folhas de cenoura
+
+1 dente de alho
+
+50g de queijo parmesão
+
+50g de nozes ou castanhas
+
+Azeite
+
+Sal e pimenta a gosto<br><br>
+
+Modo de Preparo:<br>
+
+
+
+
+Bata todos os ingredientes no liquidificador ou processador.
+
+Ajuste a consistência com azeite.
+
+Use como molho para massas ou pães.</p>
+
+  </div>
+
+  <div class="holder" style=" display: flex;justify-content: center;">
+
+    <img class="imagemr" src="assets/tortapao.jpg" alt="" style="width: 30%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Torta de Pão Amanhecido</b><br>
+
+Ingredientes:<br>
+
+
+
+
+Pão amanhecido
+
+3 ovos
+
+2 xícaras de leite
+
+1 xícara de queijo ralado
+
+Restos de frios (presunto, queijo, etc.)
+
+Sal e pimenta a gosto<br><br>
+
+Modo de Preparo:<br>
+
+
+
+
+Pique o pão e coloque em uma tigela.
+
+Misture os ovos batidos, leite, queijo ralado e frios.
+
+Tempere com sal e pimenta.
+
+Asse em forno pré-aquecido a 180°C até dourar.</p>
+
+  </div>
+
+  <div class="holder" style=" display: flex;justify-content: center;">
+
+    <img class="imagemr" src="assets/risotoabobora.jpg" alt="" style="width: 30%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Risoto de Casca de Abóbora</b><br>
+
+Ingredientes:<br>
+
+
+
+
+Cascas de abóbora
+
+1 xícara de arroz arbóreo
+
+1 cebola
+
+1 dente de alho
+
+1 litro de caldo de legumes
+
+Queijo parmesão a gosto
+
+Azeite, sal e pimenta<br><br>
+
+Modo de Preparo:<br>
+
+
+
+
+Refogue a cebola e o alho no azeite.
+
+Adicione o arroz e mexa por alguns minutos.
+
+Acrescente as cascas de abóbora picadas.
+
+Vá adicionando o caldo de legumes aos poucos, mexendo sempre.
+
+Finalize com queijo parmesão.</p>
+
+  </div>
+
+  <div class="holder" style=" display: flex;justify-content: center;">
+
+    <img class="imagemr" src="assets/bolotalo.jpg" alt="" style="width: 30%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Bolinho de Talos</b><br>
+
+Ingredientes:<br>
+
+
+
+
+Talos de couve, brócolis, etc.
+
+1 ovo
+
+1 xícara de farinha de trigo
+
+Sal e pimenta a gosto
+
+Óleo para fritar<br><br>
+
+Modo de Preparo:<br>
+
+
+
+
+Cozinhe e pique os talos.
+
+Misture com o ovo e a farinha.
+
+Tempere e frite em óleo quente.</p>
+
+  </div>
+
+  <div class="holder" style=" display: flex;justify-content: center;">
+
+    <img class="imagemr" src="assets/docemelancia.jpg" alt="" style="width: 30%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Doce de Casca de Melancia</b><br>
+
+Ingredientes:<br>
+
+
+
+
+Cascas de melancia
+
+1 xícara de açúcar
+
+Suco de 1 limão<br><br>
+
+Modo de Preparo:<br>
+
+
+
+
+Rale a parte branca da casca.
+
+Cozinhe com açúcar e suco de limão até obter a consistência desejada.</p>
+
+  </div>
+
+  <div class="holder" style=" display: flex;justify-content: center;">
+
+    <img class="imagemr" src="assets/farofabanana.jpg" alt="" style="width: 30%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Farofa de Casca de Banana</b><br>
+
+Ingredientes:<br>
+
+
+
+
+Cascas de banana picadas
+
+1 cebola
+
+2 dentes de alho
+
+Farinha de mandioca
+
+Sal, pimenta e azeite<br><br>
+
+Modo de Preparo:<br>
+
+
+
+
+Refogue a cebola e o alho no azeite.
+
+Adicione as cascas de banana e cozinhe por alguns minutos.
+
+Acrescente a farinha de mandioca e tempere a gosto.</p>
+
+  </div>
+
+<br>
+
+<br>
+
+<br>
+
 <footer>
+
 <div class='rodape'><nav>
+
   <div class="div1">
+
       <p><strong>Nutri</strong>Ação</p>
+
     </div>
+
     <div class="div2">
+
             <a href="#">Termos de Serviço |</a>
+
             <a href="#">Política de Privacidade</a>    
+
   </div>
+
     <div class="div3"><p>
+
   <ion-icon name="logo-youtube"></ion-icon>
+
   <ion-icon name="logo-instagram"></ion-icon>
+
   <ion-icon name="logo-facebook"></ion-icon></p>
+
   </div>
+
   </nav>
+
 </div>
+
 <div class="rodape2">
+
     <P>Feito por <i>GPROGM</i></P>
+
   </div>
+
   </footer>
-<script  type = "module"  src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"> </script> 
+
+<script  type = "module"  src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"> </script>
+
 <script  nomodule  src = "https://unpkg .com/ionicons@7.1.0/dist/ionicons/ionicons.js"> </script>
+
 </body>
+
 </html>
-  
 
   
--- educaprodicas.php
+++ educaprodicas.php
@@ -1,91 +1,262 @@
 <!DOCTYPE html>
+
 <head>
+
  <meta charset="UTF-8">
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>NutriAcao</title>
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" href="style/style.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer"
+ />
+
   <link rel="stylesheet" href="style/style.css">
+
 </head>
+
 <body>
+
 <div class="navbar">
+
     <div class="logo-container">
+
       <img src="assets/logo.jpeg" alt="Logo" class="logo-img">
+
       <span class="company-name"><strong>Nutri</strong>Ação</span>
+
       
+
     </div>
+
     <ul class="nav-links">
+
       <li><a href="home.php">Home</a></li>
+
       <li><a href="quem-somos.php">Quem Somos</a></li>
+
       <li><a href="educa_noticias.php">Educa</a></li>
+
       <li><a href="junte-se.php">Junte-se a Nós</a></li>
+
     </ul>
+
   </div>
+
   <div style="display: flex;justify-content: end;align-items: end;">
+
             
+
             <div class="caixa-pesquisa" style="width:400px ;
+
     height: 50px;
+
     position: relative;
+
     box-shadow:0 0 10px rgba(0, 0, 0, 0.34);border-radius:25px">
+
                 <input type="text" placeholder="Digite aqui..." style="height: 100%;
+
   width:100% ;
+
   border-radius: 25px;
+
   outline: none;
+
   border: none;
+
   padding-left: 20px;
+
   font-size: 18px;">
+
                 <label for="check" class="iconp" style="position:absolute;right:0;top:0;height:100%;width:50px;background-color:#fff;font-size:18px;text-align:center;line-height:50px;background-color:#712584;border-radius:0 25px 25px;color:#fff">
+
                     <i class="fas fa-search"></i>
+
                 </label>
+
             </div>
+
 </div>
-  <div class="title">
-    <h1><strong>Nutri</strong>Ação educa</h1>
-  </div>
-  
-  <a href="https://radioriomarfm.com.br/acoes-tentam-evitar-desperdico-de-alimentos-em-feiras-de-manaus/#:~:text=De%20acordo%20com%20a%20Secretaria,a%20import%C3%A2ncia%20de%20evitar%20desperd%C3%ADcio."><img  src="assets/noticiaprincipa.png" alt="" width=100%;></a>
+
+<br>
+
   <br>
+
+<img  src="assets/educapro.png" alt="" width=100%;>
+
   <br>
+
+  <br>
+
 <div class='buttons'>
+
     <a class="btn btn-primary" href="educapro.php" role="button"><ion-icon name="earth-outline"></ion-icon> Notícias</a>
+
     <a class="btn btn-primary" href="ecucaproreceitas.php" role="button"><ion-icon name="restaurant-outline"></ion-icon> Receitas</a>
+
     <a class="btn btn-primary" href="educaprodicas.php" role="button"><ion-icon name="newspaper-outline"></ion-icon> Dicas</a>
+
     
+
   </div>
+
   <div class="holder" style=" display: flex;justify-content: center;">
-  <a href="https://casavogue.globo.com/lazer-e-cultura/comida-bebida/noticia/2023/12/como-aproveitar-sobras-ceia.ghtml">
-    <img class="imagemr" src="assets/dica1.png" alt="" style="width: 80%;border-radius: 5%;margin: 10px;margin-left: 15%;margin-top: 3%;position: relative;text-align: center;">
-  </a>
-  <a href="https://www.cegantin.com.br/noticia/cozinha-industrial-6-dicas-de-seguranca-12.html">
-    <img class="imagemr" src="assets/dica2.png" alt="" style="width: 80%;border-radius: 5%;margin: 10px;margin-left: 15%;margin-top: 3%;position: relative;text-align: center;">
-  </a>
-</div>
-  
-<footer>
-<div class='rodape'><nav>
-  <div class="div1">
-      <p><strong>Nutri</strong>Ação</p>
-    </div>
-    <div class="div2">
-            <a href="#">Termos de Serviço |</a>
-            <a href="#">Política de Privacidade</a>    
+
+    <img class="imagemr" src="assets/horta.jpg" alt="" style="width: 30%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Horta de Compostagem</b><br>
+
+    Materiais Necessários:<br>
+
+
+
+
+Caixote de madeira ou recipiente para compostagem
+
+<br>Restos de comida (vegetais, cascas de frutas, borras de café, cascas de ovos trituradas, etc.)
+
+<br>Material seco (folhas secas, aparas de grama, serragem, papelão picado)
+
+<br>Água<br><br>
+
+Instruções:<br>
+
+Montagem do Composto: Comece colocando uma camada de material seco no fundo do seu caixote de compostagem. Isso ajudará a melhorar a drenagem e aeração.
+
+
+
+
+<br>Adição dos Restos de Comida: Em seguida, adicione uma camada de restos de comida. Isso inclui vegetais, frutas, borras de café, cascas de ovos (trituradas) e outros resíduos orgânicos da sua cozinha.
+
+
+
+
+<br>Adição de Material Seco: Cubra os restos de comida com outra camada de material seco. Isso ajuda a evitar odores e a manter a relação correta de carbono e nitrogênio para uma boa decomposição.
+
+
+
+
+<br>Umidade: Regue o composto conforme necessário para manter a umidade. Ele deve estar úmido, mas não encharcado.
+
+
+
+
+<br>Aeração: De vez em quando, revolva o composto para promover a aeração e acelerar o processo de decomposição.
+
+
+
+
+<br>Tempo de Decomposição: Dependendo das condições, o composto estará pronto para ser usado em sua horta dentro de algumas semanas a alguns meses. Quando estiver escuro, cheirar a terra e não reconhecer mais os materiais originais, está pronto para uso.
+
+<br><br>
+
+Observações Importantes:
+
+
+
+
+<br>Evite adicionar restos de carne, produtos lácteos, óleos ou alimentos processados ao composto, pois eles podem atrair pragas e retardar o processo de decomposição.
+
+
+
+
+<br>Mantenha uma proporção equilibrada de material úmido (restos de comida) e material seco (folhas, papelão) para garantir uma decomposição eficiente.
+
+
+
+
+<br>Se você não tiver espaço para uma compostagem tradicional, considere usar uma composteira doméstica, que é menor e pode ser usada em ambientes internos.
+
+</p>
+
+  </div>s
+
+  <div class="holder" style=" display: flex;justify-content: center;">
+
+    <img class="imagemr" src="assets/planoref.jpg" alt="" style="width: 30%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Planeje Suas Refeições</b><br>
+
+Antes de fazer compras, planeje suas refeições da semana e faça uma lista de compras baseada nisso. Isso ajuda a evitar compras impulsivas e desnecessárias.</p>
+
   </div>
-    <div class="div3"><p>
-  <ion-icon name="logo-youtube"></ion-icon>
-  <ion-icon name="logo-instagram"></ion-icon>
-  <ion-icon name="logo-facebook"></ion-icon></p>
+
+  <div class="holder" style=" display: flex;justify-content: center;">
+
+    <img class="imagemr" src="assets/poteher.jpeg" alt="" style="width: 30%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Armazene os alimentos corretamente</b><br>
+
+    Armazenar os alimentos corretamente pode prolongar sua vida útil. Utilize recipientes herméticos, organize sua geladeira e despensa para que os itens mais antigos sejam usados primeiro, e aprenda as melhores práticas para conservar frutas, verduras e outros
+ alimentos.</p>
+
   </div>
-  </nav>
-</div>
-<div class="rodape2">
-    <P>Feito por <i>GPROGM</i></P>
+
+  <div class="holder" style=" display: flex;justify-content: center;">
+
+    <img class="imagemr" src="assets/doacomida.jpeg" alt="" style="width: 45%;border-radius: 5%;margin: 5px;margin-left: 5%;margin-top: 3%;position: relative;text-align: center;">
+
+    <p style="margin-top:5%; margin-left:2%;"><b>Doe alimentos</b><br>
+
+    
+
+Se você tem alimentos que não vai conseguir consumir antes que estraguem, doe-os para amigos, vizinhos ou organizações que ajudam pessoas em situação de vulnerabilidade.</p>
+
   </div>
-  </footer>
-<script  type = "module"  src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"> </script> 
-<script  nomodule  src = "https://unpkg .com/ionicons@7.1.0/dist/ionicons/ionicons.js"> </script>
-</body>
-</html>
-  
+
+  <br><br><br>
 
   
+
+<footer>
+
+<div class='rodape'><nav>
+
+  <div class="div1">
+
+      <p><strong>Nutri</strong>Ação</p>
+
+    </div>
+
+    <div class="div2">
+
+            <a href="#">Termos de Serviço |</a>
+
+            <a href="#">Política de Privacidade</a>    
+
+  </div>
+
+    <div class="div3"><p>
+
+  <ion-icon name="logo-youtube"></ion-icon>
+
+  <ion-icon name="logo-instagram"></ion-icon>
+
+  <ion-icon name="logo-facebook"></ion-icon></p>
+
+  </div>
+
+  </nav>
+
+</div>
+
+<div class="rodape2">
+
+    <P>Feito por <i>GPROGM</i></P>
+
+  </div>
+
+  </footer>
+
+<script  type = "module"  src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"> </script>
+
+<script  nomodule  src = "https://unpkg .com/ionicons@7.1.0/dist/ionicons/ionicons.js"> </script>
+
+</body>
+
+</html>
